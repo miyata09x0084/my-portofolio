8bc081c6925491c46d7d74388df74a8d3f823f7a
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2021-08-05 23:02:38 +0900

    面を作成する

--- main.js
+++ main.js
@@ -11,29 +11,48 @@
   );
   const renderer = new THREE.WebGLRenderer(
   )
-	console.log(scene);
-  console.log(camera);
-  console.log(renderer);
 
   renderer.setSize(innerWidth, innerHeight)
+  renderer.setPixelRatio(devicePixelRatio)
   document.body.appendChild(renderer.domElement)
 
   const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
-  const material = new THREE.MeshBasicMaterial({color: 0x00FF00})
-  console.log(boxGeometry)
-  console.log(material)
-
+  const material = new THREE.MeshBasicMaterial({color: 0x165e83})
   const mesh = new THREE.Mesh(boxGeometry, material)
-  console.log(mesh)
 
   scene.add(mesh)
-
   camera.position.z = 5
+
+  const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10)
+  console.log(planeGeometry);
+  const planeMaterial = new THREE.MeshPhongMaterial({
+    color: 0xcaca82,
+    side: THREE.DoubleSide,
+    flatShading: THREE.FlatShading
+  })
+  const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)
+  const light = new THREE.DirectionalLight(0xffffff, 1)
+  light.position.set(0, 0, 1)
+  scene.add(light)
+
+  // console.log()
+  const {array} = planeMesh.geometry.attributes.position
+
+  for (let i = 0; i  < array.length; i += 3) {
+    const x = array[i]
+    const y = array[i + 1]
+    const z = array[i + 2]
+
+    // array[i + 2] = z + Math.random()
+  }
+
+  scene.add(planeMesh)
 
   function animate() {
     requestAnimationFrame(animate)
     renderer.render(scene, camera)
-    mesh.rotation.x += 0.02
+    mesh.rotation.x += 0.005
+    mesh.rotation.y += 0.005
   }
 
   animate()
