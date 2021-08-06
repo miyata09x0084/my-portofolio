fa7fdf96ba349038f8784209b38ea526eb5f2a12
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2021-08-06 09:13:24 +0900

    視点を切り替えるためのライブラリを読み込む

--- main.js
+++ main.js
@@ -1,6 +1,45 @@
   // Find the latest version by visiting https://cdn.skypack.dev/three.
 
   import * as THREE from 'https://cdn.skypack.dev/three@0.126.1';
+  import {OrbitControls} from 'https://cdn.skypack.dev/-/three@v0.126.1-LpZka0bKUQ3PqqIzhvFC/dist=es2020,mode=raw/examples/jsm/controls/OrbitControls.js';
+  import * as dat from 'dat.gui';
+
+  console.log(OrbitControls)
+
+  const gui = new dat.GUI()
+  const world = {
+    plane: {
+      width: 10,
+      height: 10,
+      widthSegments: 10,
+      heightSegments: 10,
+    }
+  }
+
+  gui.add(world.plane, 'width', 1, 20).onChange(generatePlane)
+  gui.add(world.plane, 'height', 1, 20).onChange(generatePlane)
+  gui.add(world.plane, 'widthSegments', 1, 20).onChange(generatePlane)
+  gui.add(world.plane, 'heightSegments', 1, 20).onChange(generatePlane)
+
+  function generatePlane() {
+    planeMesh.geometry.dispose()
+    planeMesh.geometry = new THREE.PlaneGeometry(
+      world.plane.width,
+      world.plane.height,
+      world.plane.widthSegments,
+      world.plane.heightSegments
+    )
+
+    const {array} = planeMesh.geometry.attributes.position
+
+    for (let i = 0; i  < array.length; i += 3) {
+      const x = array[i]
+      const y = array[i + 1]
+      const z = array[i + 2]
+
+      array[i + 2] = z + Math.random()
+    }
+  }
 
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(
@@ -16,14 +55,9 @@
   renderer.setPixelRatio(devicePixelRatio)
   document.body.appendChild(renderer.domElement)
 
-  const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
-  const material = new THREE.MeshBasicMaterial({color: 0x165e83})
-  const mesh = new THREE.Mesh(boxGeometry, material)
-
-  scene.add(mesh)
   camera.position.z = 5
 
-  const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10)
+  const planeGeometry = new THREE.PlaneGeometry(10, 10, 10, 10)
   console.log(planeGeometry);
   const planeMaterial = new THREE.MeshPhongMaterial({
     color: 0xcaca82,
@@ -35,24 +69,11 @@
   light.position.set(0, 0, 1)
   scene.add(light)
 
-  // console.log()
-  const {array} = planeMesh.geometry.attributes.position
-
-  for (let i = 0; i  < array.length; i += 3) {
-    const x = array[i]
-    const y = array[i + 1]
-    const z = array[i + 2]
-
-    array[i + 2] = z + Math.random()
-  }
-
   scene.add(planeMesh)
 
   function animate() {
     requestAnimationFrame(animate)
     renderer.render(scene, camera)
-    mesh.rotation.x += 0.005
-    mesh.rotation.y += 0.005
   }
 
   animate()
